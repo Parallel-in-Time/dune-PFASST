e0fa8b8f4e991cb87f77b3bcdf92daddd63e8135
ruthschoebel <ruth.schoebel@rwth-aachen.de>
2017-07-21 23:05:46 +0200

    new version of main

--- src/examples/fischer/FE_sdcFP.cpp
+++ src/examples/fischer/FE_sdcFP.cpp
@@ -10,17 +10,6 @@
 #include "fischer_sweeper.hpp"
 
 
-//////////////////////////////////////////////////////////////////////////////////////
-//
-// Compiletimeparameter
-//
-//////////////////////////////////////////////////////////////////////////////////////
-
-//const size_t DIM = 1;            //Raeumliche Dimension des Rechengebiets ruth_dim
-
-//const size_t BASIS_ORDER = 1;    //maximale Ordnung der Lagrange Basisfunktionen
-
-//////////////////////////////////////////////////////////////////////////////////////
 
 using namespace pfasst::examples::heat_FE;
 
@@ -29,94 +18,86 @@ using std::shared_ptr;
 using encap_traits_t = pfasst::encap::dune_vec_encap_traits<double, double, 1>; 
 
 
-      using FE_function = Dune::Functions::PQkNodalBasis<GridType::LevelGridView, BASE_ORDER>;  
-      using sweeper_t = test<dune_sweeper_traits<encap_traits_t, BASE_ORDER, DIMENSION>,   FE_function >;
-      using pfasst::transfer_traits;
-      using pfasst::contrib::SpectralTransfer;
-      using pfasst::SDC;
-      using pfasst::quadrature::QuadratureType;
-      using heat_FE_sdc_t = SDC<SpectralTransfer<transfer_traits<sweeper_t, sweeper_t, 1>>>;
+using FE_function = Dune::Functions::PQkNodalBasis<GridType::LevelGridView, BASE_ORDER>;  
+using sweeper_t = test<dune_sweeper_traits<encap_traits_t, BASE_ORDER, DIMENSION>,   FE_function >;
 
-      shared_ptr<heat_FE_sdc_t> run_sdc(const size_t nelements, const size_t basisorder, const size_t dim, const size_t nnodes,
-                                       const QuadratureType& quad_type, const double& t_0,
-                                       const double& dt, const double& t_end, const size_t niter)
-      {
-        using pfasst::quadrature::quadrature_factory;
+using pfasst::transfer_traits;
+using pfasst::contrib::SpectralTransfer;
+using pfasst::SDC;
+using pfasst::quadrature::QuadratureType;
+using heat_FE_sdc_t = SDC<SpectralTransfer<transfer_traits<sweeper_t, sweeper_t, 1>>>;
 
-        auto sdc = std::make_shared<heat_FE_sdc_t>();
-	
-        auto FinEl   = make_shared<fe_manager>(nelements,1); 
+using pfasst::config::get_value;
+using pfasst::quadrature::QuadratureType;
+using pfasst::quadrature::quadrature_factory;
 
-        //auto sweeper = std::make_shared<sweeper_t>(FinEl->get_basis2(), 0, FinEl->get_grid());
-        auto sweeper = std::make_shared<sweeper_t>(FinEl->get_basis(0), 0, FinEl->get_grid());
-
-        sweeper->quadrature() = quadrature_factory<double>(nnodes, quad_type);
-
-        sweeper->set_abs_residual_tol(1e-6);
-        sdc->add_sweeper(sweeper);
-
-        sdc->set_options();
-
-        sdc->status()->time() = t_0;
-        sdc->status()->dt() = dt;
-        sdc->status()->t_end() = t_end;
-        sdc->status()->max_iterations() = niter;
-
-        sdc->setup();
-
-        sweeper->initial_state() = sweeper->exact(sdc->get_status()->get_time());
-        Dune::BlockVector<Dune::FieldVector<double, 1> > w = sweeper->initial_state()->data();
-	
-        std::cout << "vor run " <<std::endl;
-        
-        sdc->run();
-
-        sdc->post_run();
+using pfasst::examples::heat_FE::test;
 
 
 
-	std::cout <<  "fein" << std::endl;
-        auto naeherung = sweeper->get_end_state()->data();
-        auto exact     = sweeper->exact(t_end)->data();
-        //for (int i=0; i< sweeper->get_end_state()->data().size(); i++){
-          //std::cout << naeherung[i] << "   " << exact[i] << std::endl;
-        //}
-
-	  typedef Dune::BlockVector<Dune::FieldVector<double, 1> > VectorType;
-          VectorType x = sweeper->get_end_state()->data();
-	  VectorType y = sweeper->exact(t_end)->data();
-        
-	  
-	  x-=y;
-	 // x/=y.infinity_norm();
-	  
-	  //sweeper->states()[sweeper->get_states().size()-1]->scaled_add(-1.0 , sweeper->exact(t_end));
-          //std::cout << sweeper->states()[sweeper->get_states().size()-1]->norm0()<<  std::endl ;
-
-	  sweeper->get_end_state()->scaled_add(-1.0 , sweeper->exact(t_end));
-    {
-      auto A = sweeper->get_A_dune();
-      A*=-1.0;
-      std::cout << "A[0][0]: " << A[0][0] << std::endl;
-      std::cout << "A[1][1]: " << A[1][1] << std::endl;
-      auto tmp = sweeper->get_end_state()->data();
-      A.mv(sweeper->get_end_state()->data(), tmp);
-      std::cout << "H1 error: " << tmp*sweeper->get_end_state()->data() << std::endl;
+int main(int argc, char** argv) {
       
-    }
-    std::cout << sweeper->get_end_state()->norm0()<<  std::endl ;
+    Dune::MPIHelper::instance(argc, argv);
+    
+    
+    // read in the parameter from terminal and make initial setup
+    
+    pfasst::init(argc, argv, sweeper_t::init_opts);
 
+    const size_t nelements = get_value<size_t>("num_elements", 180);    // spacial dimension: number of grid points per dimension on the coase level
+    
+    const double t_0 = 0.0;                                             // left point of the time intervall is zero 
+    const double dt = get_value<double>("dt", 0.05);                    // size of timesteping
+    double t_end = get_value<double>("tend", 0.1);                      // right point of the time intervall  
+    const size_t nnodes = get_value<size_t>("num_nodes", 3);            // time intervall: number of sdc quadrature points
+    const QuadratureType quad_type = QuadratureType::GaussRadau;        // quadrature type
+    const size_t niter = get_value<size_t>("num_iters", 10);            // maximal number of sdc iterations
+
+
+    auto sdc = std::make_shared<heat_FE_sdc_t>();
 	
-      ofstream f;
-	  stringstream ss;
-	  ss << nelements;
-	  string s = "solution_sdc/" + ss.str() + ".dat";
-	  f.open(s, ios::app | std::ios::out );
-      f << nelements << " " << dt << " "<< sweeper->get_end_state()->norm0()<< endl;
-	  //f << nelements << " " << dt << " "<< x.infinity_norm()<< endl;
+    auto FinEl   = make_shared<fe_manager>(nelements,1); 
 
-	  f.close();
+    auto sweeper = std::make_shared<sweeper_t>(FinEl->get_basis(0), 0, FinEl->get_grid());
+    sweeper->quadrature() = quadrature_factory<double>(nnodes, quad_type);
+    
+    sdc->add_sweeper(sweeper);
+    sdc->set_options();
+    sdc->status()->time() = t_0;
+    sdc->status()->dt() = dt;
+    sdc->status()->t_end() = t_end;
+    sdc->status()->max_iterations() = niter;
+    sdc->setup();
 
+    // initial value is given by the exact solution on time step 0
+    sweeper->initial_state() = sweeper->exact(sdc->get_status()->get_time());
+
+    sdc->run();
+    //do not need a post run for GaussRadau nodes not sure about that should ask robert
+    //sdc->post_run();
+
+    //auto naeherung = sweeper->get_end_state()->data();
+    //auto exact     = sweeper->exact(t_end)->data();
+
+    // calculate the error of the simulation
+    auto A = sweeper->get_A_dune(); A*=-1.0; //get the stiffnessmatrix, which is defined negative (need to change that!) 
+    auto tmp = sweeper->get_end_state()->data();
+    sweeper->get_end_state()->scaled_add(-1.0 , sweeper->exact(t_end));
+    A.mv(sweeper->get_end_state()->data(), tmp);
+    std::cout << "error in H1 norm: " << tmp*sweeper->get_end_state()->data() << std::endl;
+    std::cout << "error in infinity norm: " << sweeper->get_end_state()->norm0()<<  std::endl ;
+
+    bool output=false;
+    if (output){
+        //write in a file the error
+        ofstream f;
+        stringstream ss << nelements;
+        string s = "solution_sdc/" + ss.str() + ".dat";
+        f.open(s, ios::app | std::ios::out );
+        f << nelements << " " << dt << " "<< sweeper->get_end_state()->norm0()<< endl;
+        f.close();
+
+        //write in a file the number of sdc iterations
         ofstream ff;
         stringstream sss;
         sss << nelements <<  "_iter";
@@ -124,55 +105,12 @@ using encap_traits_t = pfasst::encap::dune_vec_encap_traits<double, double, 1>;
         ff.open(st, ios::app | std::ios::out );
         auto iter = sdc->_it_per_step;
         for (const auto &line : iter) {
-          ff << dt <<"  " << line << std::endl;
+            ff << dt <<"  " << line << std::endl;
         }
-
         ff.close();
-	
-	
-        return sdc;
-      }
+    }	    
 
-
-  int main(int argc, char** argv) {
-      
-          //MPIHelper::instance(argc,argv);  
-    Dune::MPIHelper::instance(argc, argv);
-    using pfasst::config::get_value;
-    using pfasst::quadrature::QuadratureType;
-    using pfasst::examples::heat_FE::test;
-
-    using sweeper_t = test<pfasst::examples::heat_FE::dune_sweeper_traits<encap_traits_t, BASE_ORDER, DIMENSION>, FE_function>;
-
-    pfasst::init(argc, argv, sweeper_t::init_opts);
-
-    const size_t nelements = get_value<size_t>("num_elements", 180); //Anzahl der Elemente pro Dimension
-    const size_t nnodes = get_value<size_t>("num_nodes", 3);
-    const QuadratureType quad_type = QuadratureType::GaussRadau;
-    const double t_0 = 0.0;
-    const double dt = get_value<double>("dt", 0.05);
-    double t_end = get_value<double>("tend", 0.1);
-    size_t nsteps = get_value<size_t>("num_steps", 0);
-    if (t_end == -1 && nsteps == 0) {
-      ML_CLOG(ERROR, "USER", "Either t_end or num_steps must be specified.");
-      throw std::runtime_error("either t_end or num_steps must be specified");
-    } else if (t_end != -1 && nsteps != 0) {
-      if (!pfasst::almost_equal(t_0 + nsteps * dt, t_end)) {
-        ML_CLOG(ERROR, "USER", "t_0 + nsteps * dt != t_end ("
-                               << t_0 << " + " << nsteps << " * " << dt << " = " << (t_0 + nsteps * dt)
-                               << " != " << t_end << ")");
-        throw std::runtime_error("t_0 + nsteps * dt != t_end");
-      }
-    } else if (nsteps != 0) {
-      t_end = t_0 + dt * nsteps;
-    }
-    
-    std::cout << "nsteps = " << nsteps << std::endl;
-    
-    const size_t niter = get_value<size_t>("num_iters", 10);
-
-    run_sdc(nelements, BASE_ORDER, DIMENSION, nnodes, quad_type, t_0, dt, t_end, niter);
-
-  }
+ 
+}
 
 
